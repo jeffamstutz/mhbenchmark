a51ab6cf08a81dce563cff0fb43c52ce113c60a4
Jefferson Amstutz <jeff.amstutz@survice.com>
2014-12-03 22:09:47 -0500

    scenes are now expected to all be in one directory

--- run_tests.sh
+++ run_tests.sh
@@ -11,39 +11,39 @@ echo "--------------------"
 echo " "
 echo "-->conf:"
 echo " "
-./benchmark $1/conf/conf.obj $2 $3 images/conf_$4.png
+./benchmark $1/conf.obj $2 $3 images/conf_$4.png
 
 echo " "
 echo "-->fair:"
 echo " "
-./benchmark $1/fair/fair.obj $2 $3 images/fair_$4.png
+./benchmark $1/fair.obj $2 $3 images/fair_$4.png
 
 echo " "
 echo "-->hairball:"
 echo " "
-./benchmark $1/hairball/hairball.obj $2 $3 images/hairball_$4.png
+./benchmark $1/hairball.obj $2 $3 images/hairball_$4.png
 
 echo " "
 echo "-->pplant:"
 echo " "
-./benchmark $1/pplant/pplant.obj $2 $3 images/pplant_$4.png
+./benchmark $1/pplant.obj $2 $3 images/pplant_$4.png
 
 echo " "
 echo "-->sanm:"
 echo " "
-./benchmark $1/sanm/sanm.obj $2 $3 images/sanm_$4.png
+./benchmark $1/sanm.obj $2 $3 images/sanm_$4.png
 
 echo " "
 echo "-->sibe:"
 echo " "
-./benchmark $1/sibe/sibe.obj $2 $3 images/sibe_$4.png
+./benchmark $1/sibe.obj $2 $3 images/sibe_$4.png
 
 echo " "
 echo "-->tank:"
 echo " "
-./benchmark $1/tank/tank.obj $2 $3 images/tank_$4.png
+./benchmark $1/tank.obj $2 $3 images/tank_$4.png
 
 echo " "
 echo "-->truck:"
 echo " "
-./benchmark $1/truck/truck.obj $2 $3 images/truck_$4.png
+./benchmark $1/truck.obj $2 $3 images/truck_$4.png

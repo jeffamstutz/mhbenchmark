1d21b458e873e424829d12612b0a62837234ea96
Jefferson Amstutz <jeff.amstutz@survice.com>
2014-12-03 21:44:39 -0500

    setup script to run on JCGT scenes and hairball

--- run_tests.sh
+++ run_tests.sh
@@ -9,11 +9,41 @@ echo " MH Benchmark suite"
 echo "--------------------"
 
 echo " "
+echo "-->conf:"
+echo " "
+./benchmark $1/conf/conf.obj $2 $3 images/conf_$4.png
+
+echo " "
+echo "-->fair:"
+echo " "
+./benchmark $1/fair/fair.obj $2 $3 images/fair_$4.png
+
+echo " "
 echo "-->hairball:"
 echo " "
 ./benchmark $1/hairball/hairball.obj $2 $3 images/hairball_$4.png
 
 echo " "
-echo "-->powerplant:"
+echo "-->pplant:"
 echo " "
-./benchmark $1/powerplant/powerplant.obj $2 $3 images/powerplant_$4.png
+./benchmark $1/pplant/pplant.obj $2 $3 images/pplant_$4.png
+
+echo " "
+echo "-->sanm:"
+echo " "
+./benchmark $1/sanm/sanm.obj $2 $3 images/sanm_$4.png
+
+echo " "
+echo "-->sibe:"
+echo " "
+./benchmark $1/sibe/sibe.obj $2 $3 images/sibe_$4.png
+
+echo " "
+echo "-->tank:"
+echo " "
+./benchmark $1/tank/tank.obj $2 $3 images/tank_$4.png
+
+echo " "
+echo "-->truck:"
+echo " "
+./benchmark $1/truck/truck.obj $2 $3 images/truck_$4.png
